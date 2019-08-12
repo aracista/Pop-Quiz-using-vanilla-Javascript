47f63ad9418ade69cb4612d223e26064ff3702c5
Your Name <ian.rizki2@gmail.com>
2019-08-12 17:33:24 +0700

    90% without status answer

--- Scripts/script.js
+++ Scripts/script.js
@@ -7,17 +7,19 @@ questions[4] = "questions 5";
 questions[5] = "questions 6";
 questions[6] = "questions 7"; 
 
-function oldList() {
+function menu() {
+	var kandidat = localStorage.getItem("candidate");
+	kandidat = kandidat ? kandidat.split(',') : [];
 	var oldListAnswer  = confirm("Do you wish to continue the old list?");
 	if (oldListAnswer == true) {
 		newCandidateAnswer();
 	} else{
-		localStorage.removeItem("candidate");
+		localStorage.clear();
 		newCandidateAnswer();
 	}
 	listPending();
+	passedCandidate();
 	setTimer();
-
 }
 
 function newCandidateAnswer() {
@@ -35,80 +37,140 @@ function newCandidateAnswer() {
 		}
 	}while(newCandidateAnswer === true)
 }
-
 function setTimer(){
-var newCandidateAnswer = prompt("How many seconds do you wish to set your timer ?");
-var waktu = newCandidateAnswer;
-	setInterval(function() {
+	 var newCandidateAnswer = prompt("How many seconds do you wish to set your timer ?");
+	 var waktu = newCandidateAnswer;
+	 countdown(waktu);
+}
 
+function countdown(waktu){
+	var interval = waktu;
+
+	var inter = setInterval(function(){
 		var kandidat = localStorage.getItem("candidate");
 		kandidat = kandidat ? kandidat.split(',') : [];
 		var passed = localStorage.getItem("passed");
 		passed = passed ? passed.split(',') : [];
-		if(waktu == 0) {
+
+		if(interval == 0){
+			var beginButton = document.querySelector("#begin-button");
+			var correctButton = document.querySelector("#correct-button");
+			var wrongButton = document.querySelector("#wrong-button");
+			var question = document.getElementById("question");
+
+			document.querySelector(".sentence").style.display = "flex";
+			beginButton.style.display = "inline";
+			document.querySelector("#correct-button").style.display = "none";
+			document.querySelector("#wrong-button").style.display = "none";
+			document.querySelector("#question").style.display = "none";
+
+
 			var randomKandidat = get_random(kandidat);
-			var newTextNode = document.createTextNode(randomKandidat);
+			
 			var candidate = document.getElementById("candidate");
-			candidate.appendChild(newTextNode);
+			var spanCandidate = document.createElement("span");
+			spanCandidate.textContent = randomKandidat;
+			candidate.appendChild(spanCandidate);
 			document.querySelector(".modal-wrapper").style.display = "flex";
 
-			correctButton.addEventListener("click", function(event){
-   				document.querySelector(".modal-wrapper").style.display = "none";
-   				passed.push(randomKandidat);
-				localStorage.setItem("passed", passed.toString());
-				var updateArray = arrayRemove(kandidat,randomKandidat);
-				console.log(updateArray);
-				localStorage.setItem("candidate", updateArray.toString());
+			if (kandidat.length > 0) {
+				beginButton.addEventListener("click", function(event){
+				deleteChild("#candidate");
+				var questionTextNode = get_question(questions);
+				var question = document.getElementById("question");
+				question.textContent = questionTextNode;
+				document.querySelector(".sentence").style.display = "none";
+				beginButton.style.display = "none";
+			    document.querySelector("#question").style.display = "flex";
+			     correctButton.style.display = "inline";
+			     wrongButton.style.display = "inline";
+			  });
 
-  			});
+				correctButton.addEventListener("click", function(event){
+	   				document.querySelector(".modal-wrapper").style.display = "none";
+	   				passed.push(randomKandidat);
+					localStorage.setItem("passed", passed.toString());
+					var updateArray = arrayRemove(kandidat,randomKandidat);
+					console.log(updateArray);
+					localStorage.setItem("candidate", updateArray.toString());
+					interval = waktu;
+					listPending();
+					passedCandidate();
+				});
+
+
+
+				wrongButton.addEventListener("click", function(event){
+	   				document.querySelector(".modal-wrapper").style.display = "none";
+	   				passed.push(randomKandidat);
+					localStorage.setItem("passed", passed.toString());
+					var updateArray = arrayRemove(kandidat,randomKandidat);
+					console.log(updateArray);
+					localStorage.setItem("candidate", updateArray.toString());
+					interval = waktu;
+					listPending();
+					passedCandidate();
+				});
+			}else{
+				clearInterval(inter);
+				alert("game over");
+				document.querySelector(".modal-wrapper").style.display = "none";
+				document.getElementById("seconds-left").innerHTML = 0;
+			}
+			
+
+		}else {
+			if (interval >= 0) {
+				document.getElementById("seconds-left").innerHTML = interval;
+			}
+			else {
+				document.getElementById("seconds-left").innerHTML = 0;
+			}
+			
 
 		}
-		if (waktu > -1) {
-			document.getElementById("seconds-left").innerHTML = waktu;
-		}
-
-		if (waktu > -1) {
-			waktu--;
-		}
-		
-	}, 1000);
+		interval --;
+	},1000);
 }
+
 
 function listPending(){
 	var kandidat = localStorage.getItem("candidate");
 	kandidat = kandidat ? kandidat.split(',') : [];
-
+	deleteChild("#pending");
 	for(index = 0; index < kandidat.length; index++){
 	  console.log(kandidat[index]);
 	  	var newParagraph = document.createElement("span");
 		var newTextNode = document.createTextNode(kandidat[index]);
 		newParagraph.appendChild(newTextNode);
-		var leftContainerNode = document.getElementsByClassName("left-container");
-		leftContainerNode[0].appendChild(newParagraph);
+		var pendingNode = document.querySelector("#pending");
+		pendingNode.appendChild(newParagraph);
 	}
 }
 
-get_random = function (kandidat) {
+function passedCandidate(){
+	var passed = localStorage.getItem("passed");
+	passed = passed ? passed.split(',') : [];
+	deleteChild("#passed");
+	for(index = 0; index < passed.length; index++){
+	  console.log(passed[index]);
+	  	var newParagraph = document.createElement("span");
+		var newTextNode = document.createTextNode(passed[index]);
+		newParagraph.appendChild(newTextNode);
+		var passedNode = document.querySelector("#passed");
+		passedNode.appendChild(newParagraph);
+	}
+}
+
+function get_random(kandidat) {
   return kandidat[Math.floor((Math.random()*kandidat.length))];
 }
 
-get_question = function (questions) {
+function get_question(questions) {
   return questions[Math.floor((Math.random()*questions.length))];
 } 
 
-var beginButton = document.querySelector("#begin-button");
-var correctButton = document.querySelector("#correct-button");
-var wrongButton = document.querySelector("#wrong-button");
-beginButton.addEventListener("click", function(event){
-	var questionTextNode = document.createTextNode(get_question(questions));
-	var question = document.getElementById("question");
-	question.appendChild(questionTextNode);
-	document.querySelector(".sentence").style.display = "none";
-	beginButton.style.display = "none";
-    document.querySelector("#question").style.display = "flex";
-     correctButton.style.display = "inline";
-     wrongButton.style.display = "inline";
-  });
+
 
   function arrayRemove(array, value){
   	return array.filter(function(element){
@@ -116,8 +178,19 @@ beginButton.addEventListener("click", function(event){
   	});
   }
 
+  function deleteChild(selector){
+  	var parent = document.querySelector(selector);
+  	var child = parent.lastElementChild;
+  	while(child){
+  		parent.removeChild(child);
+  		child = parent.lastElementChild;
+  	}
+  }
 
-oldList()
+
+menu();
+
+
 
 
 
